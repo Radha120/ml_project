bff5ab4d233f6de04584fb12c9a51e4037c2532b
Radha krishna <arkishna2003@gmail.com>
2026-01-22 15:04:11 +0530

    Hyperparameter training

--- ml_project/src/utils.py
+++ ml_project/src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import customException
 def save_object(file_path,obj):
     try:
@@ -13,17 +14,26 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise customException(e,sys)
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
-            model.fit(x_train,y_train)
-            y_train_pred=model.predict(x_train)
-            y_test_pred=model.predict(x_test)
-            trian_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
-            report[list(models.keys())[i]]=test_model_score
-        return report
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+
+            if model_name == "CatBoosting Regressor":
+                model.fit(x_train, y_train)
+            else:
+               para = param.get(model_name, {})
+               gs = GridSearchCV(model, para, cv=3)
+               gs.fit(x_train, y_train)
+               model.set_params(**gs.best_params_)
+               model.fit(x_train, y_train)
+               y_train_pred=model.predict(x_train)
+               y_test_pred=model.predict(x_test)
+               trian_model_score=r2_score(y_train,y_train_pred)
+               test_model_score=r2_score(y_test,y_test_pred)
+               report[list(models.keys())[i]]=test_model_score
+            return report
     except Exception as e:
         raise customException(e,sys)
