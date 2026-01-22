1caf8466e5e6a2ce688e2b3936390bbf17b468f1
Radha krishna <arkishna2003@gmail.com>
2026-01-22 11:42:20 +0530

    Data Transformation done

--- ml_project/src/components/data_ingestion.py
+++ ml_project/src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
+from src.components.data_transfromation import DataTransformation
+from src.components.data_transfromation import DataTransformationconfig
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
@@ -17,7 +19,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('NoteBook\Data\stud.csv')
+            df = pd.read_csv('C:/Users/A/Desktop/projects/ml_project/NoteBook/Data/stud.csv')
             logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -34,4 +36,6 @@ class DataIngestion:
             raise customException(e,sys)
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()  
+    train_data,test_data=obj.initiate_data_ingestion()  
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
